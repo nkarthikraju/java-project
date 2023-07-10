9531feccf54899f2c4f6d66574955deb011d8ad2
nkarthikraju <35051284+nkarthikraju@users.noreply.github.com>
2023-07-10 18:07:31 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins"); 
-        System.out.println("Jenkins is a CICD toolkk"); 
+        System.out.println("Jenkins is a CICD toholkk"); 
     }
 }
